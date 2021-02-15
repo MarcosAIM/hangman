672ea8af910509c07c5e079ab9a6c5a961aed97b
mai20 <mai20@txstate.edu>
2021-02-14 19:35:22 -0600

    comments for wordGenerator

--- src/word_generator.py
+++ src/word_generator.py
@@ -1,6 +1,16 @@
 from random import choice
 
+'''
+class Word:
+    attributes:
+        word_bank:
+            list of words for use in hangman game.
 
+    functions:
+        pick_word:
+            params:none
+            returns:random word chosen from word_bank
+'''
 class WordGenerator:
     def __init__(self):
         self.word_bank = ['resolution',
@@ -25,4 +35,4 @@ class WordGenerator:
                            'industry', ]
 
     def pick_word(self):
-        return choice(self.word_bank)
+        return choice(self.word_bank) #random word
